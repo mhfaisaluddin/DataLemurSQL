a8069ff859d2916377c99226c33f8f1f7aa4432d
Muh.Hanif Faisaluddin <109103941+mhfaisaluddin@users.noreply.github.com>
2022-11-09 21:08:24 +0700

    Add files via upload

---
+++ Easy/Facebook_Page With No Likes.sql
@@ -0,0 +1,7 @@
+-- Query to return the page IDs of all the FB pages that don't have any likes
+
+SELECT p.page_id
+FROM pages AS p LEFT JOIN page_likes AS pl
+ON p.page_id = pl.page_id
+WHERE pl.user_id IS NULL
+ORDER BY p.page_id ASC
