908fb4ee9bd4892a9c4dd284ea175031f37f90a9
Muh.Hanif Faisaluddin <109103941+mhfaisaluddin@users.noreply.github.com>
2022-11-09 21:03:05 +0700

    Add files via upload

---
+++ Easy/LinkedIn_Data Science Skills.sql
@@ -0,0 +1,7 @@
+-- Query to list the candidates who have all the required skills
+
+SELECT candidate_id FROM candidates
+WHERE skill IN ('Python','Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
