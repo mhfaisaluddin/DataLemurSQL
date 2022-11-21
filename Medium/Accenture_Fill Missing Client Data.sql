bc43bcd62b50138a553d478e443304fd725016d0
Muh.Hanif Faisaluddin <109103941+mhfaisaluddin@users.noreply.github.com>
2022-11-21 19:48:32 +0700

    Add files via upload

---
+++ Medium/Accenture_Fill Missing Client Data.sql
@@ -0,0 +1,13 @@
+-- Query to returns the product catalog with the missing data filled in
+
+WITH fill_products AS(
+  SELECT product_id, category, name, 
+    COUNT(category) OVER(ORDER BY product_id) AS category_group
+  FROM products
+)
+
+SELECT product_id,
+  FIRST_VALUE(category) 
+    OVER(PARTITION BY category_group ORDER BY product_id) AS category,
+  name
+FROM fill_products
