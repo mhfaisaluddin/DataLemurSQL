55f794cae5c67e7ca8e1ba496955b0b777d49490
Muh.Hanif Faisaluddin <109103941+mhfaisaluddin@users.noreply.github.com>
2022-12-06 18:40:23 +0700

    Add files via upload

---
+++ Hard/Amazon_Maximize Prime Item Inventory.sql
@@ -0,0 +1,42 @@
+-- Query to find the number of prime and non-prime items that can be stored in the 500,000 square feet warehouse.
+
+WITH summary AS
+(
+  SELECT 
+    item_type,
+    SUM(square_footage) AS total_sqft,
+    COUNT(*) AS item_count
+  FROM inventory
+  GROUP BY 1
+),
+prime_items AS
+(
+  SELECT 
+    DISTINCT item_type,
+    total_sqft,
+    TRUNC(500000/total_sqft,0) AS prime_item_combo,
+    (TRUNC(500000/total_sqft,0) * item_count) AS prime_item_count
+  FROM summary
+  WHERE item_type = 'prime_eligible'
+),
+non_prime_items AS
+(
+  SELECT
+    DISTINCT item_type,
+    total_sqft,
+    TRUNC(
+      (500000 - (SELECT prime_item_combo * total_sqft FROM prime_items))
+      / total_sqft, 0) * item_count AS non_prime_item_count
+  FROM summary
+  WHERE item_type = 'not_prime'
+)
+
+SELECT 
+  item_type,
+  prime_item_count AS item_count
+FROM prime_items
+UNION ALL
+SELECT
+  item_type,
+  non_prime_item_count AS item_count
+FROM non_prime_items;
